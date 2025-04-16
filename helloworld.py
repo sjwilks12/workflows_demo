f1421462dee601d9452600d081c1d61cdb2e5d45
Sam Wilkins <54691685+sjwilks12@users.noreply.github.com>
2025-04-16 10:07:32 -0700

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,10 +1,10 @@
-import sys
+# import sys
 
 def main():
     print("Hello World! From Python: " + str(sys.version_info))
-    if sys.version_info >= (3, 7) and sys.version_info < (3, 8):
-        # let's make this script fail for Python 3.6
-        raise Exception('Python version 3.6.x is unsupported!')
+    # if sys.version_info >= (3, 7) and sys.version_info < (3, 8):
+    #     # let's make this script fail for Python 3.6
+    #     raise Exception('Python version 3.6.x is unsupported!')
 
 if __name__ == "__main__":
     main()
